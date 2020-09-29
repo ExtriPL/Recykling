5ae575268bcc09b8f58c464f1e00cd580b49c6a8
Mruwawa <mruwa.wa@o2.pl>
2020-09-29 20:36:53 +0200

    login.php, formularz

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        
+        <form action="login.php" method = "post">
+            <label for="nickname">Login</label><input type="text" name="nickname"><br>
+            <label for="password">Hasło<input type="password" name="password"><br>
+            <button action = "submit">ZALOGUJ</button>
+        </form>
     </body>
 </html>
