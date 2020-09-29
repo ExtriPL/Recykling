4af81e06f467d998291a242af7c090acf673405b
Jakub Boniecki <ExtriPL@gmail.com>
2020-09-29 21:22:59 +0200

    Wiadomość zwrotna

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+?>
+
 <html lang='pl'>
     <head>
         <meta charset = 'utf-8'>
@@ -9,5 +13,13 @@
             <label for="password">Hasło<input type="password" name="password"><br>
             <button action = "submit">ZALOGUJ</button>
         </form>
+
+        <?php
+            if(isset($_SESSION["back-message"]))
+            {
+                echo "<p style='color:red;'>".$_SESSION["back-message"]."</p>";
+                unset($_SESSION["back-message"]);
+            }
+        ?>
     </body>
 </html>
