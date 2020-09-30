2acfeb190a2f3d9994c0d931e2cd46f2d9f7d821
WowDoggoWow <wowdoggowow@wp.pl>
2020-09-30 17:54:08 +0200

    Odczytywanie typu odpadku z pliku

--- wasteInput.php
+++ wasteInput.php
@@ -4,36 +4,39 @@
 <meta charset="UTF-8">
 </head>
 <body>
-
-<label for="typeofWaste">Wybierz typ odpaku: </label>
-
-<select name="typeOfWaste" id="typeofWaste">
-<option value="butelka">Butelka</option>
-<option value="puszka">Puszka</option>
-</select>
-&nbsp
-<label for="amountOfWaste">Ilość odpadku: </label>
-
-<input type="number" id="amountOfWaste" name="amountOfWaste" min="1">
-
 <?php   
-$fileOptions;
-function openFile($fileName)
+$fileWithTypesName = "types.txt";
+$wasteTypeArray = [];
+
+function openFile($fileName,$arrayName)
 {
 if(file_exists($fileName))
 {
     $fileOptions = fopen($fileName,"r");
     while(!feof($fileOptions))
-    {
-        fgets($fileOptions);
+    {   
+        $line = fgets($fileOptions);
+        array_push($arrayName,$line);
     }
     fclose($fileOptions);
+    echo "<label for='typeofWaste'>Wybierz typ odpadku: </label>
+    <select name='typeOfWaste' id='typeofWaste'>";
+    foreach($arrayName as $item)
+    {
+        echo "<option value=".$item.">".$item."</option>";
+    }
+echo "</select>";
 }
 else
 {
     echo "Plik nie istnieje lub jest niepoprawny";
 }
 }
+
+openFile($fileWithTypesName,$wasteTypeArray);
+echo "&nbsp
+<label for='amountOfWaste'>Ilość odpadku: </label>
+<input type='number' id='amountOfWaste' name='amountOfWaste' min='1'>";
 
 ?>
 </body>
