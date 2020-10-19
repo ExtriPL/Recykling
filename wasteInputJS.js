8b181de60750997f2de74e1c1e8b45b050d88e41
WowDoggoWow <wowdoggowow@wp.pl>
2020-10-19 22:44:40 +0200

    Dodanie obslugi wprowadzania odpadkow

---
+++ wasteInputJS.js
@@ -0,0 +1,34 @@
+let items = {};
+
+function addItem()
+{
+    let wasteName = $("#wastecategory").children("option:selected").val();
+    let wasteQuantity = parseInt($("#wastequantity").val());
+    addToBasket(wasteName,wasteQuantity);
+    $("#data").val(JSON.stringify(items));
+}
+
+function addToBasket(itemName,itemQuantity)
+{
+    if($("#"+itemName).length)
+    {
+        updateItem(itemName,itemQuantity);
+    }
+    else
+    {
+        let item = "<div id='"+itemName+"'>"+itemName+" <span id='"+itemName+"Quantity'>"+itemQuantity+"</span></div>";
+        $("#basket").append(item);
+        items[itemName] = itemQuantity;
+    }
+}
+
+function updateItem(itemName,addQuantity)
+{
+    items[itemName] += addQuantity;
+    $("#"+itemName+"Quantity").text(items[itemName]);
+}
+
+function removeFromBasket(itemName)
+{
+    $("#"+itemName).remove();
+}
